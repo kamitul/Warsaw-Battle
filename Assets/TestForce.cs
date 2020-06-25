f0264a73c639b580af811fd765d88d61b60f42f4
Kamil Tulczyjew <kamcio819@gmail.com>
2020-06-26 01:09:41 +0200

    Added dice

---
+++ Assets/TestForce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForce : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rg;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rg.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            rg.AddTorque(new Vector3(UnityEngine.Random.Range(0, 500), UnityEngine.Random.Range(0, 500), UnityEngine.Random.Range(0, 500)));
+        }
+    }
+}
