c34118c0cfe9904c2f888eddc58ba15097dd615b
Kamil Tulczyjew <kamcio819@gmail.com>
2020-06-26 15:00:29 +0200

    Addde more implementation

---
+++ Assets/EventController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventController : MonoBehaviour
+{
+    public UnityEvent OnGameStart;
+    public UnityEvent OnGameExit;
+    public UnityEvent OnTurnEnd;
+}
