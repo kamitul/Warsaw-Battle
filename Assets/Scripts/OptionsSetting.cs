c742c3cac424eecf1d88ab317855ad49d549573e
Blazej <sentai74@gmail.com>
2020-06-28 15:46:51 +0200

    Added menu options

---
+++ Assets/Scripts/OptionsSetting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSetting : MonoBehaviour
+{
+    [SerializeField] private MouseData mouseData;
+    [SerializeField] private AudioSource audioSource;
+
+    public void SetSensitivityX(float sensX)
+    {
+        mouseData.MouseSensX = sensX;
+    }
+    public void SetSensitivityY(float sensY)
+    {
+        mouseData.MouseSensX = sensY;
+    }
+    public void SetSensitivityZ(float sensZ)
+    {
+        mouseData.MouseSensX = sensZ;
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    public void Back()
+    {
+        gameObject.SetActive(false);
+    }
+}
