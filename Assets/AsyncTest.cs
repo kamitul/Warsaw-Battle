2dd66cc79b29d08c6f0f2835075138faf5cce00d
Kamil Tulczyjew <kamcio819@gmail.com>
2020-07-25 13:10:06 +0200

    Small improvement to UI

---
+++ Assets/AsyncTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AsyncTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        List<Task> tasks = new List<Task>();
+
+        tasks.Add(Task.Run(() => transform.Rotate(20, 0, 0)));
+        tasks.Add(Task.Run(() => transform.Translate(20, 0, 0)));
+
+        await Task.WhenAll(tasks);
+
+        Debug.Log("XD");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
